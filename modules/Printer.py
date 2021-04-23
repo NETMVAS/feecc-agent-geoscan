7854b48b92f29056fd15174dec42b2c85934063f
arseniiarsenii <arseniivelichko2@gmail.com>
2021-04-23 12:51:46 +0300

    improved logging

--- modules/Printer.py
+++ modules/Printer.py
@@ -3,6 +3,13 @@ import logging
 from PIL import Image
 from brother_ql import BrotherQLRaster, conversion
 from brother_ql.backends.helpers import send
+`
+# set up logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="agent.log",
+    format="%(asctime)s %(levelname)s: %(message)s"
+)
 
 
 class Task:
@@ -14,16 +21,16 @@ class Task:
         When creating an instance of the class, it creates a task for a brother QL-800 printer to print a label with a
         qr-code passed as an argument. picname != qrpic, it contains side fields and logos (optionally)
         """
-        logging.warning("Initializing printer")
+        logging.info("Initializing printer")
 
         qr = Image.open(picname)
 
         printer = "usb://0x04f9:0x209b"  # link to device on RPI
         label_name = "62"  # that depends on paper used for printing
 
-        logging.warning("Printing...")
+        logging.info("Printing...")
         qlr = BrotherQLRaster("QL-800")
         conversion.convert(qlr, [qr], label_name, red=True)
         send(qlr.data, printer)  # this is some standard code for printing with brother label printer with python,
         # red = True means that black and red printing will be done. Only for 62 label paper
-        logging.warning("Printed!")
+        logging.info("Printed!")
